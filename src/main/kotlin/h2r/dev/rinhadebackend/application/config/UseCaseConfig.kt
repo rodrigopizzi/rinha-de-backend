65bd507e55e62fd8ae3983c3c4dadd840e89e9ca
Rodrigo <rodrigopizzi@gmail.com>
2023-09-30 17:42:55 -0300

    create endpoint fo get pessoa

--- src/main/kotlin/h2r/dev/rinhadebackend/application/config/UseCaseConfig.kt
+++ src/main/kotlin/h2r/dev/rinhadebackend/application/config/UseCaseConfig.kt
@@ -2,6 +2,7 @@ package h2r.dev.rinhadebackend.application.config
 
 import h2r.dev.rinhadebackend.domain.port.PessoaDatabasePort
 import h2r.dev.rinhadebackend.domain.usecase.CreatePessoaUseCase
+import h2r.dev.rinhadebackend.domain.usecase.GetPessoaUseCase
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,5 +11,8 @@ class UseCaseConfig {
 
     @Bean
     fun createPessoaUseCase(pessoaDatabasePort: PessoaDatabasePort) = CreatePessoaUseCase(pessoaDatabasePort)
+
+    @Bean
+    fun getPessoaUseCase(pessoaDatabasePort: PessoaDatabasePort) = GetPessoaUseCase(pessoaDatabasePort)
 
 }
--- src/main/kotlin/h2r/dev/rinhadebackend/domain/usecase/GetPessoaUseCase.kt
+++ src/main/kotlin/h2r/dev/rinhadebackend/domain/usecase/GetPessoaUseCase.kt
@@ -3,10 +3,8 @@ package h2r.dev.rinhadebackend.domain.usecase
 import h2r.dev.rinhadebackend.domain.entity.Pessoa
 import h2r.dev.rinhadebackend.domain.exception.NotFoundException
 import h2r.dev.rinhadebackend.domain.port.PessoaDatabasePort
-import org.springframework.stereotype.Service
 import java.util.*
 
-@Service
 class GetPessoaUseCase(
     private val pessoaDatabasePort: PessoaDatabasePort
 ) {
