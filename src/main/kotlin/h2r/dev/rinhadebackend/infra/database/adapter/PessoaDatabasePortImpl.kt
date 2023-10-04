c24c1276bdf2ff50f16930e4624a020e0cd520cf
Rodrigo <rodrigopizzi@gmail.com>
2023-10-03 23:03:37 -0300

    * Change termo search

--- src/main/kotlin/h2r/dev/rinhadebackend/infra/database/adapter/PessoaDatabasePortImpl.kt
+++ src/main/kotlin/h2r/dev/rinhadebackend/infra/database/adapter/PessoaDatabasePortImpl.kt
@@ -4,6 +4,9 @@ import h2r.dev.rinhadebackend.domain.entity.Pessoa
 import h2r.dev.rinhadebackend.domain.port.PessoaDatabasePort
 import h2r.dev.rinhadebackend.infra.database.document.PessoaDocument
 import h2r.dev.rinhadebackend.infra.database.repository.PessoaRepository
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.TextCriteria
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
@@ -13,7 +16,8 @@ import kotlin.jvm.optionals.getOrNull
 @Component
 @Transactional(isolation = Isolation.READ_UNCOMMITTED)
 class PessoaDatabasePortImpl(
-    private val pessoaRepository: PessoaRepository
+    private val pessoaRepository: PessoaRepository,
+    private val mongoTemplate: MongoTemplate
 ) : PessoaDatabasePort {
 
     override fun save(pessoa: Pessoa): Pessoa {
@@ -25,7 +29,9 @@ class PessoaDatabasePortImpl(
     }
 
     override fun getByTermo(termo: String): List<Pessoa> {
-        return pessoaRepository.findTop50ByTermoRegex(".*$termo.*").map { it.toDomain() }
+        val criteria = TextCriteria.forDefaultLanguage().matching(termo)
+        val query = Query.query(criteria)
+        return mongoTemplate.find(query, PessoaDocument::class.java).map { it.toDomain() }
     }
 
     override fun isApelidoExists(apelido: String): Boolean {
--- src/main/kotlin/h2r/dev/rinhadebackend/infra/database/repository/PessoaRepository.kt
+++ src/main/kotlin/h2r/dev/rinhadebackend/infra/database/repository/PessoaRepository.kt
@@ -4,6 +4,5 @@ import h2r.dev.rinhadebackend.infra.database.document.PessoaDocument
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface PessoaRepository : MongoRepository<PessoaDocument, String> {
-    fun findTop50ByTermoRegex(termo: String): List<PessoaDocument>
     fun existsByApelido(apelido: String): Boolean
 }
